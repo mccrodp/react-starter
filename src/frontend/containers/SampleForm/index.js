423505cd1eb2f61f175b3702a97bfe918e6579d3
mccrodp <info@opensourceopenmind.ninja>
2017-03-13 19:41:47 +0000

    Change to conditional const to highlight progress state issue.

--- src/frontend/containers/SampleForm/index.js
+++ src/frontend/containers/SampleForm/index.js
@@ -10,23 +10,10 @@ module.exports = function SampleForm (props) {
     return <div style={{color: 'red'}}>{props.error.toString()}</div>
   }
 
-  if (props.progress) {
-    console.log('ok');
-    return (
-      <form>
-        <label>
-          Texts:
-          <input type="text" value={props.text} /> <br />
-          Checkbox:
-          <input type="checkbox" name="vehicle" value="Bike" checked={props.checked}/> <br />
-        </label>
-        <div>{props.message}</div>
-      </form>
-    );
-  }
+  const conditional = props.progress ?
+  <div>This is a {props.message}</div> :
+  <button type="submit" value="Submit" disabled={props.disabled}>Submit</button>;
 
-else {
-  console.log('not ok');
   return (
     <form>
       <label>
@@ -35,10 +22,9 @@ else {
         Checkbox:
         <input type="checkbox" name="vehicle" value="Bike" checked={props.checked}/> <br />
       </label>
-      <button type="submit" value="Submit" disabled={props.disabled}>Submit</button>
+      {conditional}
     </form>
   );
-}
 
   return <div>Nothing loaded</div>
 }
